66a8d25b1b525e6faf42271f0c26c26c0a0ad310
rorsach <rorsach@olmdigital.com>
2017-09-14 00:22:47 -0700

    Adding preliminary localStorage support.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 (function () {
-  
-  let numberOfProblems = 30
-  let minNumDigits = 1
-  let maxNumDigits = 1
-  let allowBorrowing = false
-  let operatorType = ''
+
+  let appState = {
+    numberOfProblems: 30,
+    minNumDigits: 1,
+    maxNumDigits: 1,
+    allowBorrowing: false,
+    operatorType: '+'
+  }
+
 
   const numberOfProblemsEl = document.getElementById('numberOfProblems')
   const minDigitsEl = document.getElementById('minDigits')
@@ -19,8 +22,7 @@
     let problemArray = [];
     let problem = {}
     
-    for (let i = 0; i < numberOfProblems; i++) {
-      
+    for (let i = 0; i < appState.numberOfProblems; i++) {    
       problem = generateProblem()
       problemArray.push(problem)
     }
@@ -38,11 +40,11 @@
 
     setDigitLimits()
     
-    let numDigits = getRandomInt(minNumDigits, maxNumDigits)
+    let numDigits = getRandomInt(appState.minNumDigits, appState.maxNumDigits)
     
     for (let i = 0; i < numDigits; i++) {
       operand1 = getRandomInt(0, 9)
-      operand2 = (subtractionOperatorEl.checked && allowBorrowing) ? getRandomInt(0, 9) :  getRandomInt(0, operand1)
+      operand2 = (subtractionOperatorEl.checked && appState.allowBorrowing) ? getRandomInt(0, 9) :  getRandomInt(0, operand1)
 
       operand1String += operand1
       operand2String += operand2
@@ -62,6 +64,11 @@
   }
 
   function addListeners() {
+
+    document.addEventListener('change', (event) => {
+      console.log(event.target)
+      serializeAppState()
+    })
     
     minDigitsEl.addEventListener("change", () => {
       if (minDigitsEl.value > maxDigitsEl.value) {
@@ -107,9 +114,40 @@
     numberOfProblemsEl.addEventListener('change', (event) => {
       setNumberOfProblems(event.target.value)
     })
+  }
+
+  function serializeAppState() {
+    console.log('serializeAppState', localStorage.getItem('appState'));
+    let jsonAppStateString = JSON.stringify(appState)
+    console.log('jsonAppStateString:', jsonAppStateString) 
+    localStorage.setItem('appState', jsonAppStateString)
     
   }
 
+  function restoreAppState() {
+    console.log('restoreAppState', localStorage.getItem('appState'));
+    if (localStorage.getItem('appState')) {
+
+      let jsonAppStateString = localStorage.getItem('appState')
+      appState = JSON.parse(jsonAppStateString)
+
+      numberOfProblemsEl.value = appState.numberOfProblems
+      minDigitsEl.value = appState.minNumDigits
+      maxDigitsEl.value = appState.maxNumDigits
+      allowBorrowing.checked = appState.allowBorrowing
+
+      operatorTypeEl.forEach((radioButton) => {
+        if (radioButton.value === appState.operatorType) {
+          debugger;
+          radioButton.checked = true
+        }
+      })
+
+    } else {
+      serializeAppState()
+    }
+  }
+  
   function toggleAllowBorrowing() {
     if (subtractionOperatorEl.checked) {
       allowBorrowingLabelEl.classList.remove('hide')
@@ -134,7 +172,7 @@
       
       operand1El.innerHTML = item.operand1
       operand2El.innerHTML = item.operand2
-      operatorEl.innerHTML = operatorType
+      operatorEl.innerHTML = appState.operatorType
 
       problemEl.appendChild(operand1El)
       problemEl.appendChild(operatorEl)
@@ -148,13 +186,13 @@
   }
 
   function setDigitLimits() {
-    maxNumDigits = parseInt(maxDigitsEl.value, 10)
-    maxNumDigits = maxNumDigits > 6 ? 6 : maxNumDigits
-    minNumDigits = parseInt(minDigitsEl.value, 10)
+    appState.maxNumDigits = parseInt(maxDigitsEl.value, 10)
+    appState.maxNumDigits = appState.maxNumDigits > 6 ? 6 : appState.maxNumDigits
+    appState.minNumDigits = parseInt(minDigitsEl.value, 10)
   }
   
   function setOperatorType(value) {
-    operatorType = value
+    appState.operatorType = value
     operatorTypeEl.forEach((radioButton) => {
       if (radioButton.value === value) {
         radioButton.checked = true
@@ -165,10 +203,11 @@
   function setNumberOfProblems(value) {
     console.log('value:', value)
     value = value > 500 ? 500 : value
-    numberOfProblems = value
+    appState.numberOfProblems = value
   }
   
   function init() {
+    restoreAppState()
     addListeners()
     setOperatorType('-')
     toggleAllowBorrowing()
